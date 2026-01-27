57f050dea34b2b221b11e7c79e908aac2af4efe4
root <test@example.com>
2026-01-27 20:33:44 +0300

    edit index.js

--- VendingMachines.Web/vendingMachinesWebClient/src/router/index.js
+++ VendingMachines.Web/vendingMachinesWebClient/src/router/index.js
@@ -1,22 +1,49 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import LoginView from '../views/LoginView.vue'
+import DashboardView from '../views/DashboardView.vue'
+import MachinesView from '../views/MachinesView.vue'
+import CalendarView from '../views/CalendarView.vue'
+import ScheduleView from '../views/ScheduleView.vue'
+import ReportsView from '../views/ReportsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: '/dashboard'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/login',
+      name: 'login',
+      component: LoginView
     },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView
+    },
+    {
+      path: '/machines',
+      name: 'machines',
+      component: MachinesView
+    },
+    {
+      path: '/calendar',
+      name: 'calendar',
+      component: CalendarView
+    },
+    {
+      path: '/schedule',
+      name: 'schedule',
+      component: ScheduleView
+    },
+    {
+      path: '/reports',
+      name: 'reports',
+      component: ReportsView
+    }
   ],
 })
 
